e4b73e2f332c1399c7cbc1a4a228af1db94dd5e1
marina <marina.moreira@smarttbotter.com>
2022-12-16 12:27:55 -0300

    feat: initial config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+};
---
+++ src/views/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Home = () => (
+    <View>
+        <Text>You don't have any decks yet</Text>
+    </View>
+)
+
+export default Home
